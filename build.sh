649ff1d41f650a1b7c763339d60ba370e6a14045
edavidaja <david@rstudio.com>
2021-08-15 19:38:16 -0400

    remove legacy build pipeline

--- build.sh
+++
@@ -1,7 +0,0 @@
-yum install -y wget
-
-wget https://github.com/gohugoio/hugo/releases/download/v0.55.1/hugo_0.55.1_Linux-64bit.tar.gz
-tar -xzvf hugo_0.55.1_Linux-64bit.tar.gz
-
-echo \n############################## \nstarting hugo:\n ############################
-./hugo
